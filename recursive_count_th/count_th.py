9512937f021eb63ec34960d3da90987215c7b15a
CodingCCarpenter <Christineassists@aol.com>
2020-08-17 21:02:48 -0500

    refactor count_th.py to include elif instead of second if statement

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,13 +4,10 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    if len(word) < 2:
-        return 0
-
-    # TBC
-
-    if word[0] + word[1] =='th':
-        return 1 + count_th(word[0:])
-    else:
-        return count_th(word[0:])
     
+    if len(word) > 1 and word[0] == "t" and word[1] == "h":
+        return 1 + count_th(word[2:])
+    elif len(word) > 2:
+        return count_th(word[1:])
+    else:
+        return 0
